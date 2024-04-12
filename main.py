58d04b6e65bde8d3c55f54f5b5adfb95edf30bc4
Korkin Alexsandr <korkin.06@inbox.ru>
2024-04-12 16:02:18 +0300

    v 1.1

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ def auto_shotdown_pc() -> None:
     import psutil as ps
     import time as t
 
-    while True:
-        print("Введите команду '/start' после того как начнете загрузку файла.\n")
-        print("/help - помощь\n")
-        choice = input("Ввод: ")
-        
-        if choice.lower() == "/start":
+    cls = lambda: os.system("cls")
 
-            os.system("cls")
-            size_file = int(input("Введите размер скачиваемого файла в гигабайтах: "))
-            
-            os.system("cls")
-
-            print("Ожидание завершения скачивания файла...")
-
-            def bytes_to_mb(bytes: int) -> int:
+    def bytes_to_mb(bytes: int) -> int:
                 while bytes > 1024:
                     bytes /= 1024
                 
                 return round(bytes * 1000, 2)
 
+    while True:
+        print("Введите команду '/start' после того как начнете загрузку файла.\n")
+
+        choice = input("Ввод: ")
+        
+        if choice.lower() == "/start":
+
+            cls()
+            size_file = int(input("Введите размер скачиваемого файла в гигабайтах: "))
+            
+            cls()
+
+            print("Ожидание завершения скачивания файла...")
 
             temp_send_bytes = 0
 
@@ -30,28 +31,21 @@ def auto_shotdown_pc() -> None:
                 net_io = ps.net_io_counters()
                 send_bytes = bytes_to_mb(net_io.bytes_recv)
 
-                if abs(send_bytes - temp_send_bytes) < 0.01:
+                if abs(send_bytes - temp_send_bytes) == 0:
                     
-                    os.system("cls")
+                    cls()
                     print("Ожидание завершения записи файла на диск...")
 
                     t.sleep((size_file * 1000) // 70)
 
-                    os.system("cls")
+                    cls()
                     print("Выключение компьютера.")
 
-                    os.system("shutdown -s -t 10")
+                    # os.system("shutdown -s -t 10")
                     break
                 
                 temp_send_bytes = bytes_to_mb(net_io.bytes_recv)
                 t.sleep(5)
-        elif choice == "/help":
-            print("""
-    1. Чтобы выйти из программы нажмите клавиши Ctrl + C.
-    2. 
-
-    """)
-        
         else:
             os.system("cls")
 
